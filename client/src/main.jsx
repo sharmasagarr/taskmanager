9ac8641bd1c119b103562dcc3ff6755269146baf
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-08-29 15:22:43 +0530

    add base in vite config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/taskmanager">
       <App />
       <Toaster position="top-center" reverseOrder={false} />
     </BrowserRouter>
