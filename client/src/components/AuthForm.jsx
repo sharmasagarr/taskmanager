69d352898b6c6404ef5fdc16b847306c2779aa50
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-08-29 16:05:43 +0530

    fix some code to deploy on gt pages

--- client/src/components/AuthForm.jsx
+++ client/src/components/AuthForm.jsx
@@ -23,7 +23,7 @@ const AuthForm = () => {
     setLoading(true);
     setError('');
 
-    const baseURL = import.meta.env.VITE_API_URL;
+    const baseURL = "https://taskmanager-beige-ten.vercel.app/api";
     const url = isLogin ? `${baseURL}/users/login` : `${baseURL}/users/signup`;
 
     const payload = isLogin
