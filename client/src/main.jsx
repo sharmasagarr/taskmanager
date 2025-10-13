d0de4b6d3cb32844900bab0b23b1ed4bb9d3ddcd
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-10-14 02:54:26 +0530

    first commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/taskmanager">
+    <BrowserRouter basename="/">
       <App />
       <Toaster position="top-center" reverseOrder={false} />
     </BrowserRouter>
