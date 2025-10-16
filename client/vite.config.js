f02b4bbc9dddd1d546ce4faec6aed39c527ca764
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-10-16 21:04:30 +0530

    modified vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/taskmanager/", 
+  base: "/taskmanager/",
+  server: {
+    host: true,
+    allowedHosts: true
+  }
 })
